6a155b3d556bcdba7696674e97ee27c325f48948
Marina Ferrarez <m.framos@hotmail.com>
2020-08-15 22:01:49 -0300

    Adição do Menu

--- Vendas/Program.cs
+++ Vendas/Program.cs
@@ -6,7 +6,37 @@ namespace Vendas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int op;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(" ----- PROJETO VENDAS -----\n");
+                Console.WriteLine(" 1 Cadastrar Cliente");
+                Console.WriteLine(" 2 Listar Cliente");
+                Console.WriteLine(" 0 Sair\n");
+                Console.WriteLine(" Escolha uma opção: ");
+                op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("-- Cadastrar Cliente --");
+
+                        break;
+                    case 2:
+                        Console.WriteLine("-- Listar Clientes --");
+                        break;
+                    case 0:
+                        Console.WriteLine("-- Saindo --");
+                        break;
+                    default:
+                        Console.WriteLine("OPÇÃO INVÁLIDA, tente de novo.");
+                        break;
+                }
+                Console.WriteLine("\nAperte uma tecla para continuar ");
+                Console.ReadKey();
+            } while (op != 0);
+           
         }
     }
 }
